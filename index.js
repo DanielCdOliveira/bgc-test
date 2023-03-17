5bf554cf83c35b966f474d0168162f60c1d3e2b0
Daniel Oliveira <danielcoliv98@gmail.com>
2023-03-17 13:07:38 -0300

    feat: puppeteer scrapper

--- index.js
+++ index.js
@@ -1,6 +1,56 @@
-exports.handler = async function (event, context) {
+import chromium from "@sparticuz/chromium";
+import puppeteerExtra from "puppeteer-extra";
+export async function handler(event, context) {
+  const browser = await puppeteerExtra.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+  });
+  await page.goto("https://www.amazon.com.br/bestsellers");
+  const result = await page.$$eval(".a-begin", (nodes) => {
+    return nodes.map((node) => {
+      const category = node.querySelector("h2").innerText;
+      const detailCategory = node.querySelector("ol");
+      const bestsellersCategory = Array.from(
+        detailCategory.querySelectorAll("li")
+      );
+      const topThree = bestsellersCategory.slice(0, 3);
+      console.log(topThree);
+      const items = topThree.map((item, index) => {
+        const linkImg = item.querySelector("img").getAttribute("src");
+        const linkProduct =
+          "https://www.amazon.com.br" +
+          item.querySelector("a").getAttribute("href");
+        const description = item.innerText.split("\n");
+        const name = description[1];
+        const rating = description[2];
+        const numberEvaluations = description[3];
+        const price = description[4];
+        return {
+          position: index + 1,
+          name,
+          rating,
+          numberEvaluations,
+          price,
+          linkImg,
+          linkProduct,
+        };
+      });
+      const bestsellers = { category, items };
+      return bestsellers;
+    });
+  });
+  browser.close();
+  console.log(result);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "My first lambda" }),
+    body: JSON.stringify(result),
   };
-};
+}
