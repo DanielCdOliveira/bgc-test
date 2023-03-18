dc1ac50f47472b59a6953097a92687de5db0fc7d
Daniel Oliveira <danielcoliv98@gmail.com>
2023-03-18 12:24:36 -0300

    feat: cron job to scrap amazon bestsellers daily and save in DynamoDB

--- lambdas/endpoints/getBestSellers.js
+++ lambdas/endpoints/getBestSellers.js
@@ -1,66 +1,14 @@
-const chromium = require("chrome-aws-lambda");
 const Responses = require("../commom/API_Responses.js");
 const Dynamo = require("../commom/Dynamo.js");
 const tableName = process.env.tableName;
 module.exports.handler = async (event) => {
-  const date = new Date().toLocaleDateString("en-US").replace(/\//g, "-");
-  const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
+  const date = event.pathParameters.date;
+  const bestsellers = await Dynamo.get(date, tableName).catch((err) => {
+    console.log("DYNAMO_ERROR", err);
+    return null;
   });
-  const page = await browser.newPage();
-  await page.setViewport({
-    width: 1920,
-    height: 1080,
-  });
-  await page.goto("https://www.amazon.com.br/bestsellers");
-  await page.waitForSelector(".a-begin");
-  const result = await page.$$eval(".a-begin", (nodes) => {
-    return nodes.map((node) => {
-      const category = node.querySelector("h2").innerText;
-      const detailCategory = node.querySelector("ol");
-      const bestsellersCategory = Array.from(
-        detailCategory.querySelectorAll("li")
-      );
-      const topThree = bestsellersCategory.slice(0, 3);
-      console.log(topThree);
-      const items = topThree.map((item, index) => {
-        const linkImg = item.querySelector("img").getAttribute("src");
-        const linkProduct =
-          "https://www.amazon.com.br" +
-          item.querySelector("a").getAttribute("href");
-        const description = item.innerText.split("\n");
-        const name = description[1];
-        const rating = description[2];
-        const numberEvaluations = description[3];
-        const price = description[4];
-        return {
-          position: index + 1,
-          name,
-          rating,
-          numberEvaluations,
-          price,
-          linkImg,
-          linkProduct,
-        };
-      });
-      const bestsellers = { category, items };
-      return bestsellers;
-    });
-  });
-  browser.close();
-  const bestSellersDB = { ID: date, bestSellers: result };
-  const newBestSellersDB = await Dynamo.write(bestSellersDB, tableName).catch(
-    (err) => {
-      console.log("Error in Dynamo write", err);
-      return null;
-    }
-  );
-  if (!newBestSellersDB) {
-    return Responses._400({ message: "Failed to write bestsellers" });
+  if (!bestsellers) {
+    return Responses._404({ message: "date not registered" });
   }
-  return Responses._200(newBestSellersDB);
+  return Responses._200(bestsellers);
 };
